df0811ba4d1410bbcc2d221aec9e01be5f1187c6
DESKTOP-9QDTJ1B\hp <gaurav2393@gmail.com>
2017-07-19 07:14:04 +0530

    changes for mouse drag left and right and for zoom-in and zoom-out

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,11 @@
 
     var totalItems,
         currentIndex = 0,
-        currentActiveElement = document.getElementById('carouselItem0');
+        currentActiveElement = document.getElementById('carouselItem0'),
+        activePosition = currentActiveElement.getBoundingClientRect(),
+        activeLeftPosition = activePosition.left,
+        activeRightPosition = activePosition.right,
+        listUpdated = false;
 
     //create list array
     function createCarouselList() {
@@ -15,6 +19,9 @@
         currentActiveElement.classList.remove('active');
         currentActiveElement = document.getElementById('carouselItem'+currentIndex);
         currentActiveElement.classList.add('active');
+        activePosition = currentActiveElement.getBoundingClientRect(),
+        activeLeftPosition = activePosition.left,
+        activeRightPosition = activePosition.right;
     }
 
 
@@ -50,8 +57,38 @@
             updateList();
         })
         //mouse left event
-
-        //mouse right event
+        $('.carousel').on('dragstart', 'img', function(event){
+            activeLeftPosition = event.originalEvent.screenX;
+            listUpdated = false;
+        })
+        $('.carousel').on('drag', 'img', function(event) {
+            if(((activeLeftPosition - event.originalEvent.screenX)>$(this).width()/2)&& !listUpdated) {
+                updateCurrentIndex('right');
+                updateList();
+                listUpdated = true;
+            }
+            else if(((event.originalEvent.screenX - activeLeftPosition)>$(this).width()/2)&& !listUpdated) {
+                updateCurrentIndex('left');
+                updateList();
+                listUpdated = true;
+            }
+        })
+        //zoom in/out
+        $('.carousel').on('mousewheel', 'img', function(event) {
+            var width = $(this).width();
+            var height = $(this).height();
+            if (event.originalEvent.wheelDelta > 0) {
+                width += 20;
+                height += 20;
+            } else if (event.originalEvent.wheelDelta < 0) {
+                if (width > 20) {
+                    width -= 20;
+                    height -= 20;
+                }
+            }
+            $(this).width(width);
+            $(this).height(height);
+        })
     }
 
     function init() {
